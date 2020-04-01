9aae8572f392152d5087898620a8229971044d8a
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2020-04-01 12:57:22 +0100

    Update to 4.12
    Still doesn't work with QEMU

--- Formula/samba.rb
+++ Formula/samba.rb
@@ -48,6 +48,7 @@ class Samba < Formula
            "--without-ad-dc",
            "--without-ads",
            "--without-dnsupdate",
+           "--without-gettext",
            "--without-ldap",
            "--without-ntvfs-fileserver",
            "--without-pam",
@@ -64,11 +65,6 @@ class Samba < Formula
            "--prefix=#{prefix}"
     system "make"
     system "make", "install"
-    # The following libraries are not installed into the prefix by make install:
-    copy_file "./bin/default/lib/ldb/libldb.dylib", "#{lib}/libldb.dylib"
-    copy_file "./bin/default/lib/talloc/libtalloc.dylib", "#{lib}/libtalloc.dylib"
-    copy_file "./bin/default/lib/tdb/libtdb.dylib", "#{lib}/libtdb.dylib"
-    copy_file "./bin/default/lib/tevent/libtevent.dylib", "#{lib}/libtevent.dylib"
     sbin.install_symlink "smbd" => "samba-dot-org-smbd"
   end
 
@@ -88,42 +84,96 @@ class Samba < Formula
   end
 end
 __END__
-commit 9d47ed6850545cd9315ef01f288b39c54e882cfb
+commit a483985e25d101c1b7a0b3a0a74525427d0aa385
 Author: Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
 Date:   Fri Oct 5 09:35:40 2018 +0100
 
     Don't use sysconf(_SC_NGROUPS_MAX) MacOS
-
+    
     On MacOS sysconf(_SC_NGROUPS_MAX) always returns 16. However, this is not
     the value used by getgroups(2). MacOS uses nested groups but getgroups(2)
     will return the flattened list which can easily exceed 16 groups. In my
     testing getgroups() already returns 16 groups on a freshly installed
     system. And on a 10.14 system the root user is in more than 16 groups by
     default which makes it impossible to run smbd without this change.
-
+    
     See https://bugzilla.samba.org/show_bug.cgi?id=8773
 
+diff --git a/buildtools/wafsamba/wscript b/buildtools/wafsamba/wscript
+index f0b679257b7..a589ecdb114 100644
+--- a/buildtools/wafsamba/wscript
++++ b/buildtools/wafsamba/wscript
+@@ -497,7 +497,10 @@ struct foo bar = { .y = 'X', .x = 1 };
+     conf.CHECK_HEADERS('strings.h inttypes.h stdint.h unistd.h minix/config.h', add_headers=True)
+     conf.CHECK_HEADERS('ctype.h', add_headers=True)
+ 
+-    if sys.platform != 'darwin':
++    if sys.platform == 'darwin':
++        conf.DEFINE('_DARWIN_C_SOURCE', 1, add_to_cflags=True)
++        conf.DEFINE('_DARWIN_UNLIMITED_GETGROUPS', 1, add_to_cflags=True)
++    else:
+         conf.CHECK_HEADERS('standards.h', add_headers=True)
+ 
+     conf.CHECK_HEADERS('stdbool.h stdint.h stdarg.h vararg.h', add_headers=True)
 diff --git a/source3/lib/system.c b/source3/lib/system.c
-index 507d4a9af93..4c6808a7637 100644
+index f1265e0c43f..ef8bd56a5a9 100644
 --- a/source3/lib/system.c
 +++ b/source3/lib/system.c
-@@ -776,7 +776,18 @@ void sys_srandom(unsigned int seed)
-
+@@ -844,7 +844,18 @@ void sys_srandom(unsigned int seed)
+ 
  int groups_max(void)
  {
 -#if defined(SYSCONF_SC_NGROUPS_MAX)
 +#if defined(DARWINOS)
 +	/*
-+	 * On MacOS sysconf(_SC_NGROUPS_MAX) returns 16
-+	 * due to MacOS's group nesting. However, getgroups()
-+	 * will return a flat list and return -1 if that flat list
-+	 * exceeds the limit of 16 (which seems to be the case for
-+	 * the root user on any 10.14 system). Since the sysconf()
-+	 * constant is not related to what getgroups() uses we
-+	 * return a fixed constant here.
++	 * On MacOS sysconf(_SC_NGROUPS_MAX) returns 16 due to MacOS's group
++	 * nesting. However, The initgroups() manpage states the following:
++	 * "Note that OS X supports group mem bership in an unlimited number
++	 * of groups. The OS X kernel uses the group list stored in the process
++	 * credentials only as an initial cache.  Additional group memberships
++	 * are determined by communication between the operating system and the
++	 * opendirectoryd daemon."
 +	 */
-+	return 128;
++	return GID_MAX;
 +#elif defined(SYSCONF_SC_NGROUPS_MAX)
  	int ret = sysconf(_SC_NGROUPS_MAX);
  	return (ret == -1) ? NGROUPS_MAX : ret;
  #else
+@@ -856,8 +867,8 @@ int groups_max(void)
+  Wrap setgroups and getgroups for systems that declare getgroups() as
+  returning an array of gid_t, but actuall return an array of int.
+ ****************************************************************************/
+-
+ #if defined(HAVE_BROKEN_GETGROUPS)
++#error "Cannot build on macos with HAVE_BROKEN_GETGROUPS"
+ 
+ #ifdef HAVE_BROKEN_GETGROUPS
+ #define GID_T int
+diff --git a/source3/smbd/sec_ctx.c b/source3/smbd/sec_ctx.c
+index 5e0710e0ecb..89e9bbdaff9 100644
+--- a/source3/smbd/sec_ctx.c
++++ b/source3/smbd/sec_ctx.c
+@@ -282,7 +282,8 @@ static void set_unix_security_ctx(uid_t uid, gid_t gid, int ngroups, gid_t *grou
+ 
+ static void set_unix_security_ctx(uid_t uid, gid_t gid, int ngroups, gid_t *groups)
+ {
+-	int max = groups_max();
++	_Static_assert(NGROUPS_MAX == 16, "initgroups manpage no longer correct?");
++	int max = NGROUPS_MAX; // groups_max();
+ 
+ 	/* Start context switch */
+ 	gain_root();
+diff --git a/source3/wscript b/source3/wscript
+index 85466b493fa..662406425d2 100644
+--- a/source3/wscript
++++ b/source3/wscript
+@@ -477,6 +477,9 @@ vsyslog
+         conf.DEFINE('DARWINOS', 1)
+         conf.ADD_CFLAGS('-fno-common')
+         conf.DEFINE('STAT_ST_BLOCKSIZE', '512')
++        # getgrouplist_2() can be used to determine the real maximum number of
++        # groups on macOS since getgrouplist() is limited to 16.
++        conf.CHECK_FUNCS('getgrouplist_2')
+     elif (host_os.rfind('freebsd') > -1):
+         conf.DEFINE('FREEBSD', 1)
+         if conf.CHECK_HEADERS('sunacl.h'):
