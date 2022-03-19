6d0f6ff8ae2594d0e5e5dc9fb088cf1ec1920c04
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2022-03-19 22:34:16 +0000

    Update sail to 0.14

--- Formula/sail.rb
+++ Formula/sail.rb
@@ -1,14 +1,9 @@
 class Sail < Formula
   desc "Language for describing the instruction semantics of processors"
   homepage "https://www.cl.cam.ac.uk/~pes20/sail/"
-  url "https://github.com/rems-project/sail/archive/0.13.tar.gz"
-  sha256 "73e593b2c9f08e2ef8f1c59c7c4541fde10393dae0f38e1176efe7c508c5839d"
+  url "https://github.com/rems-project/sail/archive/0.14.tar.gz"
+  sha256 "c4c05563b51d20a442a8b203e27cfff8bfab16eda51b5bbb1cc880860ecf4347"
   head "https://github.com/rems-project/sail.git", branch: "sail2"
-
-  bottle do
-    root_url "https://dl.bintray.com/arichardson/bottles-cheri"
-    sha256 "fae53aca4cc05d59bf45c139732ac44f930ddba1870f909bc00cc3741124c9b3" => :catalina
-  end
 
   depends_on "gmp" => :build
   depends_on "opam" => :build
